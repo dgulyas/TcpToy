9cb435bfcf15adcdf9d913253dc28fa759280da3
David Gulyas <dgulyas@invidi.com>
2019-09-26 13:03:21 -0600

    TCP server and client

---
+++ TcpClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace TcpClient
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Client:");
+			var message = "IAmDataFromClient";
+			try
+			{
+				using (System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient("localhost", 9999))
+				{
+					var data = System.Text.Encoding.ASCII.GetBytes(message);
+
+					using (NetworkStream stream = client.GetStream())
+					{
+						Console.WriteLine($"Client: Sending '{message}' to server.");
+						stream.Write(data, 0, data.Length);
+
+						// Buffer to store the response bytes.
+						data = new byte[256];
+
+						// Read the first batch of the TcpServer response bytes.
+						int bytes = stream.Read(data, 0, data.Length);
+						var responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+						Console.WriteLine($"Client: Received '{responseData}'");
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+
+			Console.WriteLine("\n Press Enter to continue...");
+			Console.Read();
+		}
+
+	}
+}
---
+++ TcpServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpServer
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Server:");
+
+			TcpListener server = new TcpListener(IPAddress.Any, 9999);
+			// we set our IP address as server's address, and we also set the port: 9999
+
+			server.Start();
+
+			while (true)
+			{
+				TcpClient client = server.AcceptTcpClient();
+
+				NetworkStream ns = client.GetStream();
+
+				Console.WriteLine($"Server: Sending 'IAmDataFromServer' to server.");
+				var response = Encoding.Default.GetBytes("IAmDataFromServer");
+				ns.Write(response, 0, response.Length);     //sending the message
+
+				while (client.Connected)  //while the client is connected, we look for incoming messages
+				{
+					byte[] msg = new byte[1024];     //the messages arrive as byte array
+					ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
+					Console.WriteLine("Server: Recieved '" +  Encoding.Default.GetString(msg) + "'"); //now , we write the message as string
+				}
+			}
+
+		}
+	}
+}
